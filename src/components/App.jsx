c65a3bebb513e3effc0ff8e8ecbf8ee5daef2bd2
Patricia Sánchez <patricia.sanchez.812@gmail.com>
2025-11-20 11:02:25 +0100

    migration completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,44 @@ import "../styles/App.scss";
 function App() {
   return (
     <div>
-      <header className="header">
-        <h1 className="title">Plantilla React</h1>
-      </header>
-      <main className="main">Holis Adalabers!!!</main>
+       <header>
+      <h1>¡Cuidado con Grogu!</h1>
+    </header>
+    <main className="page">
+      <section className="board">
+        <div className="cell"><div className="grogu">👣</div></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </section>
+
+      <section>
+        <button className="dice">Lanzar Dado</button>
+        <div className="game-status">En curso</div>
+      </section>
+
+      <section className="goods-container">
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+        <div className="goods-item">🍪</div>
+      </section>
+      <section className="goods-container">
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+        <div className="goods-item">🥚</div>
+      </section>
+      <section className="goods-container">
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+        <div className="goods-item">🐸</div>
+      </section>
+      <section>
+        <button className="restart-button">Reiniciar Juego</button>
+      </section>
+    </main>
     </div>
   );
 }
