90c7e2cf3d6a4537647c326fbd909ac05dce471d
Patricia Sánchez <patricia.sanchez.812@gmail.com>
2025-12-04 11:26:55 +0100

    configure gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/promo-58-module-3-pair-1-grogu-game',
   server: {
     open: '/',
     watch: {
